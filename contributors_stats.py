8a7de7120f62bef28929f96f8a8f77f7b4484fdb
Patrice Ferrot <patrice.ferrot@gmail.com>
2018-03-30 18:10:56 +0200

    Working with multiple repos

--- contributors_stats.py
+++ contributors_stats.py
@@ -7,21 +7,22 @@ import argparse
 import re
 import csv
 import datetime
+import os.path
 
 from requests import get
 
 
 
 parser = argparse.ArgumentParser(description='Collect contributors statistics on specified GitHub repositories.')
-parser.add_argument('-t', '--tokens', type=str, nargs='+', help='valid API token for retrieving files from git.autodesk.com')
-parser.add_argument('-r', '--repositories', type=str, nargs='+', help='full URLs of all repositories')
+parser.add_argument('-f', '--file', type=str, nargs=1, help='file containing the repos to process')
 
 args = parser.parse_args()
-if not args.tokens:
-    print ('git_token not specified (use -h for details)')
+print
+if not args.file:
+    print ('file not specified (use -h for details)')
     exit(1)
-if not args.repositories:
-    print ('gitcontractors_token not specified (use -h for details)')
+elif not os.path.exists(args.file[0]):
+    print ('file does not exist: %s' % args.file[0])
     exit(1)
 
 direct_dependencies_cache = {}
@@ -50,7 +51,7 @@ def get_commit_details(scheme, host, owner, repo, commit_sha, git_token):
         if "commit" in js and "author" in js["commit"] and "date" in js["commit"]["author"]:
             commit_date = js["commit"]["author"]["date"]
         else:
-            print ("Could not find commit date in \n\n%s" % json.dumps(js, indent=4, sort_keys=True))
+            print ("    Could not find commit date in \n\n%s" % json.dumps(js, indent=4, sort_keys=True))
 
         result = {}
         result["stats"] = js["stats"]
@@ -59,7 +60,7 @@ def get_commit_details(scheme, host, owner, repo, commit_sha, git_token):
         result["date"] = commit_date
         return result
     else:
-        print ("Erreur retrieving commit (status code: %d) at %s" % (status_code, url))
+        print ("    Erreur retrieving commit (status code: %d) at %s" % (status_code, url))
         return None
 
 def populate_totals(the_array):
@@ -87,96 +88,142 @@ def populate_totals(the_array):
 
 # Show the version of CI dependencies, see https://git.autodesk.com/LocalizationServices/sbt-ci-helper
 # for more info.
-def get_rep_stats(scheme, host, owner, repo, branch, since, git_token):
-    # URL looks like https://git.autodesk.com/api/v3/repos/LocalizationServices/segs-app/contents/ciHelperDepOverrides.sbt?ref=0.1-SEGSF-320-8928289eb0-20171114022552PST
-    url = "%s%s/repos/%s/%s/commits?sha=%s%s" % (scheme, host, owner, repo, branch, "&since=%s" % since if since else "")
-    #print (url)
-    headers = \
-        {
-         'Authorization': 'token %s' % git_token
-        }
-    reply = get(url, headers=headers)
-    code = reply.status_code
+def get_rep_stats(scheme, host, owner, repo, branch, since, git_token, index_repo, total_nb_repos):
+    next_url = "%s%s/repos/%s/%s/commits?sha=%s%s (%s)" % (scheme, host, owner, repo, branch, "&since=%s" % since if since else "", "repo %d / %d" % (index_repo, total_nb_repos))
+    print ("Processing: %s" % next_url)
+    counter = 0
     result = {}
-    if code == 200:
-        out = reply.content
-        js = json.loads(out.decode('utf-8'))
-        #print (json.dumps(js, indent=4, sort_keys=True))
-        for one_js in js:
-            author_email = None
-            commit_sha = None
-            #print (json.dumps(one_js, indent=4, sort_keys=True))
-            if "author" in one_js and one_js["author"] and "login" in one_js["author"]:
-                author_login = one_js["author"]["login"]
-            if "sha" in one_js:
-                commit_sha = one_js["sha"]
+    while next_url:
+        headers = \
+            {
+             'Authorization': 'token %s' % git_token
+            }
+        reply = get(next_url, headers=headers)
+        code = reply.status_code
+        if code == 200:
+            next_url = None
+            headers = reply.headers
+            if "Link" in headers and 'rel="next"' in headers['Link']:
+                #print(headers['Link'])
+                headers_list = headers['Link'].split(",")
+                for h in headers_list:
+                    if 'rel="next"' in h:
+                        m = re.search('<(.+?)>', h)
+                        if m:
+                            next_url = m.group(1)
+                            #print ("Next URL: %s" % next_url)
 
-            one_result = {}
-            if author_login:
-                print ("Author: %s" % author_login)
-            else:
-                print ("Author could not be found in: \n\n%s" % json.dumps(one_js, indent=4, sort_keys=True))
-                author_login = "unknown"
-            one_result["author"] = author_login
-            if commit_sha:
-                print ("SHA: %s" % commit_sha)
-                one_result["sha"] = commit_sha
-                commit_details = get_commit_details(scheme, host, owner, repo, commit_sha, git_token)
-                if commit_details:
-                    print ("Date: %s" % commit_details["date"])
-                    print ("Additions: %s" % commit_details["stats"]["additions"])
-                    print ("Deletions: %s" % commit_details["stats"]["deletions"])
-                    print ("Difference: %s" % commit_details["stats"]["difference"])
-                    print ("Total: %s" % commit_details["stats"]["total"])
+            #print ("Headers: %s" % headers)
+            out = reply.content
+            js = json.loads(out.decode('utf-8'))
+            #print (json.dumps(js, indent=4, sort_keys=True))
 
-                    one_result["date"] = commit_details["date"]
-                    one_result["stats"] = commit_details["stats"]
-                    d = datetime.datetime.strptime(commit_details["date"], "%Y-%m-%dT%H:%M:%SZ")
+            for one_js in js:
+                counter = counter + 1
+                author_email = None
+                commit_sha = None
+                #print (json.dumps(one_js, indent=4, sort_keys=True))
+                if "author" in one_js and one_js["author"] and "login" in one_js["author"]:
+                    author_login = one_js["author"]["login"]
+                if "sha" in one_js:
+                    commit_sha = one_js["sha"]
 
-                    one_result['date_unix'] = unix_time_millis(d)
-                    one_result['date_formatted'] = d.strftime('%d.%m.%Y %H:%M:%S')
-                    if author_login in result:
-                        result[author_login].append(one_result)
-                    else:
-                        a = []
-                        a.append(one_result)
-                        result[author_login] = a
+                one_result = {}
 
-            else:
-                print ("Commit SHA could not be found in: \n\n%s" % json.dumps(one_js, indent=4, sort_keys=True))
+                if not author_login:
+                    print ("    Author could not be found in: \n\n%s" % json.dumps(one_js, indent=4, sort_keys=True))
+                    author_login = "unknown"
+                #else:
+                #    print ("Author: %s" % author_login)
+                one_result["author"] = author_login
+                if commit_sha:
+                    #print ("SHA: %s" % commit_sha)
+                    one_result["sha"] = commit_sha
+                    commit_details = get_commit_details(scheme, host, owner, repo, commit_sha, git_token)
+                    if commit_details:
+                        #print ("    Date: %s" % commit_details["date"])
+                        #print ("    Additions: %s" % commit_details["stats"]["additions"])
+                        #print ("    Deletions: %s" % commit_details["stats"]["deletions"])
+                        #print ("    Difference: %s" % commit_details["stats"]["difference"])
+                        #print ("    Total: %s" % commit_details["stats"]["total"])
 
-        for x in result:
-            a = result[x]
-            #print (json.dumps(a, indent=4, sort_keys=True))
-            a = sorted(a, key=lambda k: k['date_unix'], reverse=False)
-            a = populate_totals(a)
-            result[x] = a
-        return result
-    else:
-        print ("Erreur retrieving commits (status code: %d) at %s" % (status_code, url))
-        return None
+                        one_result["date"] = commit_details["date"]
+                        one_result["stats"] = commit_details["stats"]
+                        d = datetime.datetime.strptime(commit_details["date"], "%Y-%m-%dT%H:%M:%SZ")
+
+                        one_result['date_unix'] = unix_time_millis(d)
+                        one_result['date_formatted'] = d.strftime('%d.%m.%Y %H:%M:%S')
+                        if author_login in result:
+                            result[author_login].append(one_result)
+                        else:
+                            a = []
+                            a.append(one_result)
+                            result[author_login] = a
+
+                else:
+                    print ("    Commit SHA could not be found in: \n\n%s" % json.dumps(one_js, indent=4, sort_keys=True))
+
+                if (counter % 20 == 0):
+                    print ("    Nb commits processed so far: %d (last date: %s)" % (counter, one_result['date_formatted']))
+
+        else:
+            print ("    Erreur retrieving commits (status code: %d) at %s" % (status_code, url))
+            return None
+
+    print ("    Done processing commits (total nb commits processed: %d)" % counter)
+    return result
 
 
 def merge_results(r):
     result = []
     for x in r.keys():
-        print ("X: %s" % x)
-        print (json.dumps(r[x], indent=4, sort_keys=True))
+        #print ("X: %s" % x)
+        #print (json.dumps(r[x], indent=4, sort_keys=True))
         result.extend(r[x])
     return sorted(result, key=lambda k: k['date_unix'], reverse=False)
 
+# appends r2 values to r1 values.
+def combine_results(r1, r2):
+    if not r1:
+        return r2
+    else:
+        for x in r2.keys():
+            if x in r1:
+                r1[x].extend(r2[x])
+            else:
+                r1[x] = r2[x]
+        return r1
 
 
 
-result = get_rep_stats('https://', 'api.github.com', 'pferrot', 'ochothon', 'master', '2015-11-01T00:00:00Z', 'XXXXXXX')
+result = None
+to_process = []
+csv_reader = csv.reader(open(args.file[0], newline=''), delimiter=',', quotechar='|')
+for row in csv_reader:
+    to_process.append(row)
+
+if len(to_process) == 0:
+    print("No repository to process")
+    exit(1)
+
+print("Nb repos to process: %d" % len(to_process))
+
+for idx, row in enumerate(to_process, 1):
+    result = combine_results(result, get_rep_stats(row[0], row[1], row[2], row[3], row[4], row[5], row[6], idx, len(to_process)))
 
 
+# Sort and populate totals once all repos have been processed.
+for x in result:
+    a = result[x]
+    #print (json.dumps(a, indent=4, sort_keys=True))
+    a = sorted(a, key=lambda k: k['date_unix'], reverse=False)
+    a = populate_totals(a)
+    result[x] = a
 
-
-print (json.dumps(result, indent=4, sort_keys=True))
+#print (json.dumps(result, indent=4, sort_keys=True))
 
 if result and len(result) > 0:
-    with open('test.csv', 'w', newline='') as csvfile:
+    with open('contributors_stats_%s.csv' % datetime.datetime.now().strftime("%Y%m%d%H%M%S"), 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         row = []
         row.append("Date")
