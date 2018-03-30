2267af92142aeff116ff24dee98927c5930fa740
Patrice Ferrot <patrice.ferrot@gmail.com>
2018-03-30 18:36:15 +0200

    Fix ==

--- contributors_stats.py
+++ contributors_stats.py
@@ -209,7 +209,7 @@ print("Nb repos to process: %d" % len(to_process))
 
 for idx, row in enumerate(to_process, 1):
     a = get_rep_stats(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], idx, len(to_process))
-    if a === None:
+    if a == None:
         exit(1)
     result = combine_results(result, a)
 
