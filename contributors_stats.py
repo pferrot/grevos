91f64716e4609ef9059e96b1b9c5204484630c1a
Patrice Ferrot <patrice.ferrot@gmail.com>
2018-03-31 17:29:03 +0200

    Cache results + other enhancements.

--- contributors_stats.py
+++ contributors_stats.py
@@ -8,23 +8,58 @@ import re
 import csv
 import datetime
 import os.path
+import hashlib
 
 from requests import get
 
+# Need to be manually updated. Should allow to prevent using old JSON cache
+# when the schema has been modified with a new version.
+schema_version = 1
+cache_folder = 'cache'
+output_folder = 'output'
+csv_additions = True
+csv_deletions = True
+csv_differences = True
+csv_totals = True
+csv_commits = True
 
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 parser = argparse.ArgumentParser(description='Collect contributors statistics on specified GitHub repositories.')
 parser.add_argument('-f', '--file', type=str, nargs=1, help='file containing the repos to process')
 parser.add_argument('-i', '--ignore_files', type=str, nargs='*', help='ignore commits with one of those files added')
+parser.add_argument('-o', '--output_folder', type=str, nargs='?', help='folder where the generated CSV files are stored, default: \'%s\'' % output_folder)
+parser.add_argument('-c', '--cache_folder', type=str, nargs='?', help='folder where cache files are stored, default: \'%s\'' % cache_folder)
+parser.add_argument('-cc', '--csv_commits', type=str2bool, nargs='?', default=True, help='outputs nb commits in genereted CSV, default: yes')
+parser.add_argument('-ca', '--csv_additions', type=str2bool, nargs='?', default=True, help='outputs additions in genereted CSV, default: yes')
+parser.add_argument('-cd', '--csv_deletions', type=str2bool, nargs='?', default=True, help='outputs deletions in genereted CSV, default: yes')
+parser.add_argument('-cdi', '--csv_differences', type=str2bool, nargs='?', default=True, help='outputs differences (i.e. additions - deletions) in genereted CSV, default: yes')
+parser.add_argument('-ct', '--csv_totals', type=str2bool, nargs='?', default=True, help='outputs totals (i.e. additions + deletions) in genereted CSV, default: yes')
 
 args = parser.parse_args()
-print
+
+print("CSV commits: %s " % args.csv_commits)
+
 if not args.file:
     print ('file not specified (use -h for details)')
     exit(1)
 elif not os.path.exists(args.file[0]):
     print ('file does not exist: %s' % args.file[0])
     exit(1)
+if args.output_folder:
+    output_folder = args.output_folder
+if args.cache_folder:
+    cache_folder = args.cache_folder
+
+print ("Output folder: %s" % output_folder)
+print ("Cache folder: %s" % cache_folder)
+
 
 direct_dependencies_cache = {}
 recursive_dependencies_cache = {}
@@ -94,12 +129,82 @@ def populate_totals(the_array):
     else:
         return the_array
 
+def get_output_filename():
+    return 'contributors_stats_%s.csv' % datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+def get_output_filename_with_path():
+    return get_filename_with_path(get_output_filename(), output_folder)
+
+def get_filename_with_path(filename, folder):
+    return "%s%s%s" % (folder, "" if folder.endswith("/") else "/", filename)
+
+def get_cache_filename(url):
+    # Important to take args.ignore_files into account as the result depends on this parameter.
+    cache_filename = hashlib.sha1(("%s%s%d" % (url, ",".join(args.ignore_files) if args.ignore_files else "", schema_version)).encode('utf-8')).hexdigest()
+    #print("Cache filename: %s" % cache_filename)
+    return cache_filename
+
+def get_cache_filename_with_path(url):
+    return get_filename_with_path(get_cache_filename(url), cache_folder)
+
+# Creates/overwrites the file with the given json.
+def cache(url, the_json):
+    #print ("    Caching: %s" % url)
+    with open(get_cache_filename_with_path(url), 'w') as outfile:
+        json.dump(the_json, outfile)
+
+# Returns a tuple (JSON, highest_date, sha, nb_commits), where highest_date is the date of the
+# most recent commit in the JSON and sha the SHA for that most recent commit.
+# Returns None if no cache file exists.
+def load_cache(url):
+    cache_file = get_cache_filename_with_path(url)
+    if not os.path.exists(cache_file):
+        return None
+    else:
+        try:
+            with open(cache_file) as json_data:
+                d = json.load(json_data)
+                merged_results = merge_results(d)
+                nb_commits = len(merged_results)
+                highest_date = None
+                sha = None
+                if merged_results and len(merged_results) > 0:
+                    highest_date = merged_results[len(merged_results) - 1]["date"]
+                    sha = merged_results[len(merged_results) - 1]["sha"]
+
+                #print ("Found cache, date: %s\n\n%s" % (highest_date, json.dumps(d, indent=4, sort_keys=True)))
+                return (d, highest_date, sha, nb_commits)
+        except:
+            print("    Error loading cache from file %s, so ignoring file" % cache_file)
+
+
+
 
 def get_rep_stats(scheme, host, base_path, owner, repo, branch, since, git_token, index_repo, total_nb_repos):
-    next_url = "%s%s%s/repos/%s/%s/commits?sha=%s%s (%s)" % (scheme, host, base_path, owner, repo, branch, "&since=%s" % since if since else "", "repo %d / %d" % (index_repo, total_nb_repos))
-    print ("Processing: %s" % next_url)
+    next_url = "%s%s%s/repos/%s/%s/commits?sha=%s%s" % (scheme, host, base_path, owner, repo, branch, "&since=%s" % since if since else "")
+    cache_url = next_url
+    print ("Processing: %s (%s)" % (next_url, "repo %d / %d" % (index_repo, total_nb_repos)))
+
+    since_date = None
+    if since:
+        since_date = datetime.datetime.strptime(since, "%Y-%m-%dT%H:%M:%SZ")
+
     counter = 0
     result = {}
+
+    from_cache = load_cache(cache_url)
+    cache_sha = None
+    # We found something in cache and there is a date.
+    if from_cache and from_cache[1]:
+        cache_date = from_cache[1]
+        since_date = datetime.datetime.strptime(cache_date, "%Y-%m-%dT%H:%M:%SZ")
+        #print("    Cache date: %s" % cache_date)
+        next_url = "%s%s%s/repos/%s/%s/commits?sha=%s%s" % (scheme, host, base_path, owner, repo, branch, "&since=%s" % cache_date if cache_date else "")
+        result = from_cache[0]
+        cache_sha = from_cache[2]
+        counter = from_cache[3]
+        print("    Recovered %d commits from cache" % counter)
+
     while next_url:
         headers = \
             {
@@ -126,7 +231,7 @@ def get_rep_stats(scheme, host, base_path, owner, repo, branch, since, git_token
             #print (json.dumps(js, indent=4, sort_keys=True))
 
             for one_js in js:
-                counter = counter + 1
+
                 author_email = None
                 commit_sha = None
                 #print (json.dumps(one_js, indent=4, sort_keys=True))
@@ -134,6 +239,11 @@ def get_rep_stats(scheme, host, base_path, owner, repo, branch, since, git_token
                     author_login = one_js["author"]["login"]
                 if "sha" in one_js:
                     commit_sha = one_js["sha"]
+                    # Since we query 'since' the highest date from the cache, we should get the highest result
+                    # from the cache in the response. So lets ignore it so that it is not duplicated.
+                    if cache_sha and commit_sha == cache_sha:
+                        #print("    Ignoring SHA from cache: %s" % cache_sha)
+                        continue
 
                 one_result = {}
 
@@ -158,8 +268,15 @@ def get_rep_stats(scheme, host, base_path, owner, repo, branch, since, git_token
                             #print ("    Total: %s" % commit_details["stats"]["total"])
 
                             one_result["date"] = commit_details["date"]
+
                             one_result["stats"] = commit_details["stats"]
                             d = datetime.datetime.strptime(commit_details["date"], "%Y-%m-%dT%H:%M:%SZ")
+
+                            # It seems there is a bug with the GitHub API where even if the 'since' is properly set, sometimes
+                            # commits before that date are retrieved. This should avoid that.
+                            if since_date and d < since_date:
+                                #print("    Commit is before 'since' date, so ignoring: %s" % commit_sha)
+                                continue
 
                             one_result['date_unix'] = unix_time_millis(d)
                             one_result['date_formatted'] = d.strftime('%d.%m.%Y %H:%M:%S')
@@ -173,6 +290,7 @@ def get_rep_stats(scheme, host, base_path, owner, repo, branch, since, git_token
                 else:
                     print ("    Commit SHA could not be found in: \n\n%s" % json.dumps(one_js, indent=4, sort_keys=True))
 
+                counter = counter + 1
                 if (counter % 20 == 0):
                     print ("    Nb commits processed so far: %d (last date: %s)" % (counter, one_result['date_formatted']))
 
@@ -181,8 +299,11 @@ def get_rep_stats(scheme, host, base_path, owner, repo, branch, since, git_token
             return None
 
     print ("    Done processing commits (total nb commits processed: %d)" % counter)
+    cache(cache_url, result)
     return result
 
+def sort_results(r):
+    return sorted(r, key=lambda k: k['date_unix'], reverse=False)
 
 def merge_results(r):
     result = []
@@ -190,7 +311,7 @@ def merge_results(r):
         #print ("X: %s" % x)
         #print (json.dumps(r[x], indent=4, sort_keys=True))
         result.extend(r[x])
-    return sorted(result, key=lambda k: k['date_unix'], reverse=False)
+    return sort_results(result)
 
 # appends r2 values to r1 values.
 def combine_results(r1, r2):
@@ -203,7 +324,6 @@ def combine_results(r1, r2):
             else:
                 r1[x] = r2[x]
         return r1
-
 
 
 result = None
@@ -229,14 +349,16 @@ for idx, row in enumerate(to_process, 1):
 for x in result:
     a = result[x]
     #print (json.dumps(a, indent=4, sort_keys=True))
-    a = sorted(a, key=lambda k: k['date_unix'], reverse=False)
+    a = sort_results(a)
     a = populate_totals(a)
     result[x] = a
+
+
 
 #print (json.dumps(result, indent=4, sort_keys=True))
 
 if result and len(result) > 0:
-    with open('contributors_stats_%s.csv' % datetime.datetime.now().strftime("%Y%m%d%H%M%S"), 'w', newline='') as csvfile:
+    with open(get_output_filename_with_path(), 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         row = []
         row.append("Date")
@@ -246,17 +368,42 @@ if result and len(result) > 0:
         authors_pos = {}
         for author in result.keys():
             authors_pos[author] = len(authors_pos.keys()) + 1
+            if args.csv_commits:
+                row.append("%s (commits)" % author)
+            if args.csv_additions:
+                row.append("%s (additions)" % author)
+            if args.csv_deletions:
+                row.append("%s (deletions)" % author)
+            if args.csv_differences:
+                row.append("%s (difference)" % author)
+            if args.csv_totals:
+                row.append("%s (total)" % author)
 
-            row.append("Commits (%s)" % author)
-            row.append("Additions (%s)" % author)
-            row.append("Deletions (%s)" % author)
-            row.append("Difference (%s)" % author)
-            row.append("Total (%s)" % author)
+        # Add fictive 'Total' user to see general trend.
+        nb_fields_per_author = 0
+        if args.csv_commits:
+            row.append("%s (commits)" % "TOTAL")
+            nb_fields_per_author = nb_fields_per_author + 1
+        if args.csv_additions:
+            row.append("%s (additions)" % "TOTAL")
+            nb_fields_per_author = nb_fields_per_author + 1
+        if args.csv_deletions:
+            row.append("%s (deletions)" % "TOTAL")
+            nb_fields_per_author = nb_fields_per_author + 1
+        if args.csv_differences:
+            row.append("%s (difference)" % "TOTAL")
+            nb_fields_per_author = nb_fields_per_author + 1
+        if args.csv_totals:
+            row.append("%s (total)" % "TOTAL")
+            nb_fields_per_author = nb_fields_per_author + 1
 
         writer.writerow(row)
 
-        nb_fields_per_author = 5
-
+        total_nb_commits = 0
+        total_additions = 0
+        total_deletions = 0
+        total_difference = 0
+        total_total = 0
         for one_result in merge_results(result):
             the_author = one_result["author"]
 
@@ -273,11 +420,39 @@ if result and len(result) > 0:
             #row.append(one_result["stats"]["additions"])
             #row.append(one_result["stats"]["deletions"])
             #row.append(one_result["stats"]["total"])
-            row.append(one_result["total_stats_author"]["nb_commits"])
-            row.append(one_result["total_stats_author"]["additions"])
-            row.append(one_result["total_stats_author"]["deletions"])
-            row.append(one_result["total_stats_author"]["difference"])
-            row.append(one_result["total_stats_author"]["total"])
+            if args.csv_commits:
+                row.append(one_result["total_stats_author"]["nb_commits"])
+            if args.csv_additions:
+                row.append(one_result["total_stats_author"]["additions"])
+            if args.csv_deletions:
+                row.append(one_result["total_stats_author"]["deletions"])
+            if args.csv_differences:
+                row.append(one_result["total_stats_author"]["difference"])
+            if args.csv_totals:
+                row.append(one_result["total_stats_author"]["total"])
+
+
+            # Add fictive 'Total' user to see general trend.
+            total_nb_commits = total_nb_commits + 1
+            total_additions = total_additions + one_result["stats"]["additions"]
+            total_deletions = total_deletions + one_result["stats"]["deletions"]
+            total_difference = total_difference + one_result["stats"]["difference"]
+            total_total = total_total + one_result["stats"]["total"]
+
+            for x in range(authors_pos[the_author], len(authors_pos)):
+                for y in range(0, nb_fields_per_author):
+                    row.append("")
+
+            if args.csv_commits:
+                row.append(total_nb_commits)
+            if args.csv_additions:
+                row.append(total_additions)
+            if args.csv_deletions:
+                row.append(total_deletions)
+            if args.csv_differences:
+                row.append(total_difference)
+            if args.csv_totals:
+                row.append(total_total)
 
             writer.writerow(row)
 
